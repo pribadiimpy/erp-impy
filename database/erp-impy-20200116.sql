4f5dc401475498ba523426609021caedd06ca0c1
imadepurnamayasa <62276883+imadepurnamayasa@users.noreply.github.com>
2021-01-16 10:04:02 +0800

    Update erp-impy-20200116.sql

--- database/erp-impy-20200116.sql
+++ database/erp-impy-20200116.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2021 at 01:18 AM
+-- Generation Time: Jan 16, 2021 at 03:03 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -44,10 +44,71 @@ INSERT INTO `barang` (`id`, `nama`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `gedung`
+--
+
+CREATE TABLE `gedung` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `jasa`
 --
 
 CREATE TABLE `jasa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jasa`
+--
+
+INSERT INTO `jasa` (`id`, `nama`) VALUES
+(1, 'Mencuci'),
+(2, 'Mengecat'),
+(3, 'Menyiram Tanaman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lantai`
+--
+
+CREATE TABLE `lantai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lokasi`
+--
+
+INSERT INTO `lokasi` (`id`, `nama`) VALUES
+(1, 'Rumah'),
+(2, 'Toko 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ruangan`
+--
+
+CREATE TABLE `ruangan` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -63,9 +124,33 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `gedung`
+--
+ALTER TABLE `gedung`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `jasa`
 --
 ALTER TABLE `jasa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lantai`
+--
+ALTER TABLE `lantai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ruangan`
+--
+ALTER TABLE `ruangan`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -79,9 +164,33 @@ ALTER TABLE `barang`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `gedung`
+--
+ALTER TABLE `gedung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `jasa`
 --
 ALTER TABLE `jasa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `lantai`
+--
+ALTER TABLE `lantai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ruangan`
+--
+ALTER TABLE `ruangan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
