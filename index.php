2fe42938aaa94750a7a06391ae0221caaf791223
pribadiimpy <77427566+pribadiimpy@users.noreply.github.com>
2021-01-17 10:44:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title></title>
-  </head>
-  <body>
+</head>
+<body>
     <ul>
-      <li>Merek</li>
-      <li>Satuan</li>
-      <li>Barang</li>
+        <li>Pengaturan
+            <ul>
+                <li><a href="kategori.php">Kategori</a></li>
+                <li><a href="satuan.php">Satuan</a></li>
+                <li><a href="merek.php">Merek</a></li>
+            </ul>
+        </li>
+        <li>Barang
+            <ul>
+                <li><a href="barang.php">Barang</a></li>
+            </ul>
+        </li>
+        <li>Jasa
+            <ul>
+                <li><a href="jasa.php">Jasa</a></li>
+            </ul>
+        </li>
     </ul>
-  </body>
+</body>
 </html>
