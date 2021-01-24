11bf4eb838c2d34b4167173fd4f12030ca8d64ba
pribadiimpy <77427566+pribadiimpy@users.noreply.github.com>
2021-01-24 10:48:06 +0800

    Create erp-impy-20210124.sql

---
+++ database/erp-impy-20210124.sql
@@ -0,0 +1,579 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 24, 2021 at 03:47 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `erp-impy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `kode_akun` varchar(20) NOT NULL,
+  `kode_akun_induk` varchar(20) DEFAULT NULL,
+  `nama_akun` varchar(255) DEFAULT NULL,
+  `posisi_saldo` enum('D','K','') DEFAULT NULL,
+  `posisi_laporan` enum('neraca','laba_rugi') DEFAULT NULL,
+  `hapus` enum('Y','T','') DEFAULT NULL,
+  `dibuat_tgljam` datetime DEFAULT NULL,
+  `dibuat_oleh` int(11) DEFAULT NULL,
+  `dimodifikasi_tgljam` datetime DEFAULT NULL,
+  `dimodifikasi_oleh` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_satuan` int(11) NOT NULL,
+  `id_merek` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `id_kategori`, `id_satuan`, `id_merek`) VALUES
+(1, 'baju', 1, 2, 1),
+(2, 'kemeja', 1, 1, 1),
+(3, 'jeans baru', 1, 2, 1),
+(5, 'Handphone', 1, 2, 1),
+(6, 'Laptop', 1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gedung`
+--
+
+CREATE TABLE `gedung` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jasa`
+--
+
+CREATE TABLE `jasa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jasa`
+--
+
+INSERT INTO `jasa` (`id`, `nama`) VALUES
+(1, 'Mencuci'),
+(2, 'Mengecat'),
+(3, 'Menyiram Tanaman'),
+(5, 'Membuat Program Berbasis Web');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurnal_umum`
+--
+
+CREATE TABLE `jurnal_umum` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `keterangan` text NOT NULL,
+  `posting` enum('Y','T','') NOT NULL,
+  `hapus` enum('Y','T','') NOT NULL,
+  `dibuat_tgljam` datetime NOT NULL,
+  `dibuat_oleh` int(11) NOT NULL,
+  `dimodifikasi_tgljam` datetime NOT NULL,
+  `dimodifikasi_oleh` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurnal_umum_detail`
+--
+
+CREATE TABLE `jurnal_umum_detail` (
+  `id` int(11) NOT NULL,
+  `id_jurnal_umum` int(11) NOT NULL,
+  `kode_akun` varchar(20) NOT NULL,
+  `debit_kredit` enum('D','K','') NOT NULL,
+  `jumlah` decimal(30,2) NOT NULL,
+  `hapus` enum('Y','T','') NOT NULL,
+  `posting` enum('Y','T','') NOT NULL,
+  `dibuat_tgljam` datetime NOT NULL,
+  `dibuat_oleh` int(11) NOT NULL,
+  `dimodifikasi_tgljam` datetime NOT NULL,
+  `dimodifikasi_oleh` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama`) VALUES
+(1, 'Baju');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lantai`
+--
+
+CREATE TABLE `lantai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lokasi`
+--
+
+INSERT INTO `lokasi` (`id`, `nama`) VALUES
+(1, 'Rumah'),
+(2, 'Toko 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL DEFAULT 0,
+  `nama` varchar(255) NOT NULL,
+  `module` varchar(255) NOT NULL,
+  `posisi_menu` enum('menu_utama_atas','menu_utama_kiri','menu_utama_kanan','menu_utama_bawah','menu_isi_atas','menu_isi_kiri','menu_isi_kanan','menu_isi_bawah') NOT NULL DEFAULT 'menu_utama_atas',
+  `urutan` int(11) NOT NULL,
+  `aktif` enum('Y','T') NOT NULL DEFAULT 'Y'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `menu_id`, `nama`, `module`, `posisi_menu`, `urutan`, `aktif`) VALUES
+(1, 6, 'Menu', 'pengaturan/menu.php', 'menu_utama_kiri', 0, 'Y'),
+(2, 0, 'Barang', 'barang/barang.php', 'menu_utama_atas', 100, 'Y'),
+(3, 0, 'Jasa', 'jasa/jasa.php', 'menu_utama_atas', 100, 'Y'),
+(4, 0, 'Pembeli', 'pembeli/pembeli.php', 'menu_utama_atas', 50, 'Y'),
+(5, 0, 'Penjual', 'penjual/penjual.php', 'menu_utama_atas', 50, 'Y'),
+(6, 0, 'Pengaturan', '', 'menu_utama_atas', 200, 'Y'),
+(7, 0, 'Pemetaan', '', 'menu_utama_atas', 200, 'Y'),
+(8, 6, 'Organisasi', 'pengaturan/organisasi.php', 'menu_utama_kiri', 0, 'Y'),
+(9, 16, 'Gedung', 'pengaturan/gedung.php', 'menu_isi_atas', 0, 'Y'),
+(10, 17, 'Kategori', 'pengaturan/kategori.php', 'menu_isi_atas', 0, 'Y'),
+(11, 16, 'Lantai', 'pengaturan/lantai.php', 'menu_isi_atas', 0, 'Y'),
+(12, 6, 'Lokasi', 'pengaturan/lokasi.php', 'menu_utama_kiri', 0, 'Y'),
+(13, 17, 'Merek', 'pengaturan/merek.php', 'menu_isi_atas', 0, 'Y'),
+(14, 17, 'Satuan', 'pengaturan/satuan.php', 'menu_isi_atas', 0, 'Y'),
+(15, 16, 'Ruangan', 'pengaturan/ruangan.php', 'menu_isi_atas', 0, 'Y'),
+(16, 6, 'Pengaturan Gedung', '', 'menu_utama_kiri', 0, 'Y'),
+(17, 6, 'Pengaturan Barang & Jasa', '', 'menu_utama_kiri', 0, 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merek`
+--
+
+CREATE TABLE `merek` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `merek`
+--
+
+INSERT INTO `merek` (`id`, `nama`) VALUES
+(1, 'Nokia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organisasi`
+--
+
+CREATE TABLE `organisasi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `organisasi`
+--
+
+INSERT INTO `organisasi` (`id`, `nama`) VALUES
+(1, 'IMPY');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id`, `nama`) VALUES
+(1, 'impy'),
+(2, 'purnama');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id`, `nama`) VALUES
+(1, 'purnama'),
+(2, 'yasa'),
+(3, 'calon youtuber'),
+(4, 'pribadi impy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjual`
+--
+
+CREATE TABLE `penjual` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjual`
+--
+
+INSERT INTO `penjual` (`id`, `nama`) VALUES
+(1, 'pt abc'),
+(2, 'pt abc 123'),
+(3, 'rentenir'),
+(4, 'youtube');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ruangan`
+--
+
+CREATE TABLE `ruangan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satuan`
+--
+
+CREATE TABLE `satuan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `satuan`
+--
+
+INSERT INTO `satuan` (`id`, `nama`) VALUES
+(1, 'Pcs'),
+(2, 'Kilogram');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`kode_akun`) USING BTREE,
+  ADD KEY `kode_akun` (`kode_akun`) USING BTREE,
+  ADD KEY `kode_akun_induk` (`kode_akun_induk`) USING BTREE,
+  ADD KEY `nama_akun` (`nama_akun`) USING BTREE,
+  ADD KEY `posisi_saldo` (`posisi_saldo`) USING BTREE,
+  ADD KEY `posisi_laporan` (`posisi_laporan`) USING BTREE;
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gedung`
+--
+ALTER TABLE `gedung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jasa`
+--
+ALTER TABLE `jasa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jurnal_umum`
+--
+ALTER TABLE `jurnal_umum`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE,
+  ADD KEY `tanggal` (`tanggal`) USING BTREE;
+
+--
+-- Indexes for table `jurnal_umum_detail`
+--
+ALTER TABLE `jurnal_umum_detail`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `id` (`id`) USING BTREE,
+  ADD KEY `id_jurnal_umum` (`id_jurnal_umum`) USING BTREE,
+  ADD KEY `kode_akun` (`kode_akun`) USING BTREE,
+  ADD KEY `debit_kredit` (`debit_kredit`) USING BTREE,
+  ADD KEY `jumlah` (`jumlah`) USING BTREE,
+  ADD KEY `hapus` (`hapus`) USING BTREE,
+  ADD KEY `dibuat_tgljam` (`dibuat_tgljam`) USING BTREE,
+  ADD KEY `dibuat_oleh` (`dibuat_oleh`) USING BTREE,
+  ADD KEY `dimodifikasi_tgljam` (`dimodifikasi_tgljam`) USING BTREE,
+  ADD KEY `dimodifikasi_oleh` (`dimodifikasi_oleh`) USING BTREE,
+  ADD KEY `posting` (`posting`) USING BTREE;
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lantai`
+--
+ALTER TABLE `lantai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `merek`
+--
+ALTER TABLE `merek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `organisasi`
+--
+ALTER TABLE `organisasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penjual`
+--
+ALTER TABLE `penjual`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ruangan`
+--
+ALTER TABLE `ruangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `satuan`
+--
+ALTER TABLE `satuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `gedung`
+--
+ALTER TABLE `gedung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jasa`
+--
+ALTER TABLE `jasa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `jurnal_umum`
+--
+ALTER TABLE `jurnal_umum`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jurnal_umum_detail`
+--
+ALTER TABLE `jurnal_umum_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lantai`
+--
+ALTER TABLE `lantai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `merek`
+--
+ALTER TABLE `merek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `organisasi`
+--
+ALTER TABLE `organisasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `penjual`
+--
+ALTER TABLE `penjual`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ruangan`
+--
+ALTER TABLE `ruangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `satuan`
+--
+ALTER TABLE `satuan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
